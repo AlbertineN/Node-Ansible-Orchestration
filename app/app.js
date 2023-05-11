77244af6bf2edad0124fc32073ea2b9821a2434f
AlbertineN <albertinengingan@gmail.com>
2023-05-11 10:58:23 +0300

    Updated the playbook with the correct path to the forever command for the project to work

--- app/app.js
+++ app/app.js
@@ -5,9 +5,9 @@
 var express = require('express');
 var app = express();
 
-// Respond to requests for / with 'Hello Moringa Students'.
+// Respond to requests for / with 'Hello from Albertine Nginga Devops'.
 app.get('/', function(req, res){
-    res.send('Hello Moringa Students!');
+    res.send('Hello Albertine Nginga Devops!');
 });
 
 // Listen on port 80 (like a true web server).
